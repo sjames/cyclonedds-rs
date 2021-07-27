51f8fbab3a025770960fd559e0fb3c01815f2fd8
Sojan James <sojan.james@gmail.com>
2021-07-27 20:13:10 +0530

    Intermediate checkin

--- src/deserializer.rs
+++ src/deserializer.rs
@@ -1,16 +1,11 @@
 // Rust deserializer for CycloneDDS.
 // See discussion at https://github.com/eclipse-cyclonedds/cyclonedds/issues/830
 
-use cdr::{CdrBe, Infinite};
-use serde::__private::ser;
+use cdr::{Bounded, CdrBe, Infinite};
+use serde::{Deserialize, __private::ser, de::DeserializeOwned};
 use serde_derive::{Deserialize, Serialize};
-use std::{
-    ffi::CStr,
-    fmt::format,
-    mem::{self, MaybeUninit},
-    ops::Deref,
-};
-
+use std::{ffi::{CStr, c_void}, fmt::format, marker::PhantomData, mem::{self, MaybeUninit}, ops::{Add, Deref}};
+use std::io::prelude::*;
 use cyclonedds_sys::*;
 
 //const mod_path : String = format!("{}::TopicStruct\0",  module_path!());
@@ -20,14 +15,16 @@ struct TopicStruct {
     a: u32,
 }
 
-struct SerType {
+#[repr(C)]
+struct SerType<T> {
     sertype: ddsi_sertype,
+    _phantom : PhantomData<T>
 }
 
-impl SerType {
-    fn new<T>() -> Box<SerType> 
+impl <'a,T>SerType<T> {
+    fn new() -> Box<SerType<T>> 
     where T: Topic {
-        Box::<SerType>::new(SerType {
+        Box::<SerType<T>>::new(SerType {
             sertype: {
                 let mut sertype = std::mem::MaybeUninit::uninit();
                 unsafe {
@@ -41,7 +38,16 @@ impl SerType {
                     sertype.assume_init()
                 } 
             },
+            _phantom : PhantomData,
         })
+    }
+
+    fn ref_from_ddsi_sertype(sertype: *const ddsi_sertype) -> &'a mut SerType<T> {
+        unsafe {
+            // The sertype is really a Sertype<T>. Should be safe to typecast 
+            let ptr = sertype as *mut  SerType<T>;
+            &mut *ptr
+        }
     }
 }
 
@@ -115,7 +121,7 @@ extern "C" fn free_samples<T>(
     }
 }
 
-unsafe extern "C" fn free_sertype(sertype: *mut cyclonedds_sys::ddsi_sertype) {
+unsafe extern "C" fn free_sertype<T>(sertype: *mut cyclonedds_sys::ddsi_sertype) {
     ddsi_sertype_fini(sertype) ;
 
     let _sertype_ops = Box::<ddsi_sertype_ops>::from_raw((&*sertype).ops as *mut ddsi_sertype_ops );
@@ -123,8 +129,97 @@ unsafe extern "C" fn free_sertype(sertype: *mut cyclonedds_sys::ddsi_sertype) {
     // this sertype is always constructed in Rust. During destruction,
     // the Box takes over the pointer and frees it when it goes out
     // of scope.
-    let sertype = sertype as *mut SerType;
-    Box::<SerType>::from_raw(sertype);
+    let sertype = sertype as *mut SerType<T>;
+    Box::<SerType<T>>::from_raw(sertype);
+}
+
+ // create ddsi_serdata from a fragchain
+ unsafe extern "C" fn serdata_from_fragchain<T> (
+    sertype: *const ddsi_sertype,
+    kind: u32,
+    mut fragchain: *const nn_rdata,
+    size: u64,
+) -> *mut ddsi_serdata 
+where T: DeserializeOwned {
+    //make it a reference for convenience
+    let off : u32 = 0;
+    let size = size as usize;
+    let fragchain_ref = &*fragchain;
+
+    let mut serdata = SerData::<T>::new(sertype, kind);
+    
+    assert_eq!(fragchain_ref.min, 0);
+    assert!(fragchain_ref.maxp1 >= off);
+
+    // The scatter gather list
+    let mut sg_list = Vec::new();
+
+    while !fragchain.is_null()  {
+        let fragchain_ref = &*fragchain;
+        if fragchain_ref.maxp1 > off {
+            let payload = nn_rmsg_payload_offset(fragchain_ref.rmsg, nn_rdata_payload_offset(fragchain));
+            let src = payload.add((off - fragchain_ref.min) as usize);
+            let n_bytes = fragchain_ref.maxp1 - off;
+            sg_list.push(std::slice::from_raw_parts(src, n_bytes as usize));
+
+        }
+        fragchain = fragchain_ref.nextfrag;
+    }
+
+    // make a reader out of the sg_list
+    let reader = SGReader::new(sg_list);
+    if let Ok(decoded) = cdr::deserialize_from::<_,T,_>(reader, Bounded(size as u64) ) {
+        let sample = std::sync::Arc::new(decoded);
+        //store the deserialized sample in the serdata. We don't need to deserialize again
+        serdata.maybe_sample = Some(sample);
+    } else {
+        println!("Deserialization error!");
+        return std::ptr::null_mut();
+    }
+
+    // convert into raw pointer and forget about it (for now). Cyclone will take ownership.
+    let ptr = Box::into_raw(serdata);
+    // only we know this ddsi_serdata is really of type SerData
+    ptr as *mut ddsi_serdata
+}
+
+
+unsafe extern "C" fn serdata_from_iov<T>(
+    sertype: *const ddsi_sertype,
+    kind: u32,
+    niov: u64,
+    iov: *const iovec,
+    size: u64,
+) -> *mut ddsi_serdata {
+    let size = size as usize;
+    let niov = niov as usize;
+
+    let serdata = SerData::<T>::new(sertype, kind);
+
+    let iovs =
+        std::slice::from_raw_parts(iov as *mut *const cyclonedds_sys::iovec, niov as usize);
+
+    let iov_slices: Vec<&[u8]> = iovs
+        .iter()
+        .map(|iov| {
+            let iov = &**iov;
+            let slice =
+                std::slice::from_raw_parts(iov.iov_base as *const u8, iov.iov_len as usize);
+            slice
+        })
+        .collect();
+
+    // convert into raw pointer and forget about it as ownership is passed into cyclonedds
+    let ptr = Box::into_raw(serdata);
+    // only we know this ddsi_serdata is really of type SerData
+    ptr as *mut ddsi_serdata
+}
+
+unsafe extern "C" fn free_serdata<T>(serdata: *mut ddsi_serdata) {
+    // the pointer is really a *mut SerData
+    let ptr = serdata as *mut SerData<T>;
+    let _data = Box::from_raw(ptr);
+    // _data goes out of scope and frees the SerData. Nothing more to do here.
 }
 
 // test
@@ -137,7 +232,7 @@ impl Topic for TopicStruct {
         Box::new(ddsi_sertype_ops {
             version: Some(ddsi_sertype_v0),
             arg: std::ptr::null_mut(),
-            free: Some(free_sertype),
+            free: Some(free_sertype::<Self>),
             zero_samples: Some(zero_samples::<Self>),
             realloc_samples: Some(realloc_samples::<Self>),
             free_samples: Some(free_samples::<Self>),
@@ -154,8 +249,8 @@ impl Topic for TopicStruct {
         Box::new(ddsi_serdata_ops {
             eqkey: todo!(),
             get_size: todo!(),
-            from_ser: Some(Self::serdata_from_fragchain),
-            from_ser_iov: Some(Self::serdata_from_iov),
+            from_ser: Some(serdata_from_fragchain::<Self>),
+            from_ser_iov: Some(serdata_from_iov::<Self>),
             from_keyhash: todo!(),
             from_sample: todo!(),
             to_ser: todo!(),
@@ -164,7 +259,7 @@ impl Topic for TopicStruct {
             to_sample: todo!(),
             to_untyped: todo!(),
             untyped_to_sample: todo!(),
-            free: Some(Self::free_serdata),
+            free: Some(free_serdata::<Self>),
             print: todo!(),
             get_keyhash: todo!(),
         })
@@ -174,7 +269,7 @@ impl Topic for TopicStruct {
 
 impl TopicStruct {
     pub fn create_type() -> *mut ddsi_sertype {
-        let sertype = SerType::new::<TopicStruct>();
+        let sertype = SerType::<TopicStruct>::new();
         let ptr = Box::into_raw(sertype);
         ptr as *mut ddsi_sertype
      }
@@ -199,88 +294,24 @@ impl TopicStruct {
 
     }
 
-    unsafe extern "C" fn serdata_from_iov(
-        sertype: *const ddsi_sertype,
-        kind: u32,
-        niov: u64,
-        iov: *const iovec,
-        size: u64,
-    ) -> *mut ddsi_serdata {
-        let size = size as usize;
-        let niov = niov as usize;
+   
 
-        let serdata = SerData::new(sertype, kind, size);
+   
 
-        let iovs =
-            std::slice::from_raw_parts(iov as *mut *const cyclonedds_sys::iovec, niov as usize);
-
-        let iov_slices: Vec<&[u8]> = iovs
-            .iter()
-            .map(|iov| {
-                let iov = &**iov;
-                let slice =
-                    std::slice::from_raw_parts(iov.iov_base as *const u8, iov.iov_len as usize);
-                slice
-            })
-            .collect();
-
-        // convert into raw pointer and forget about it as ownership is passed into cyclonedds
-        let ptr = Box::into_raw(serdata);
-        // only we know this ddsi_serdata is really of type SerData
-        ptr as *mut ddsi_serdata
-    }
-
-    // create ddsi_serdata from a fragchain
-    unsafe extern "C" fn serdata_from_fragchain(
-        sertype: *const ddsi_sertype,
-        kind: u32,
-        fragchain: *const nn_rdata,
-        size: u64,
-    ) -> *mut ddsi_serdata {
-        //make it a reference for convenience
-        let size = size as usize;
-        let fragchain = &*fragchain;
-        let serdata = SerData::new(sertype, kind, size);
-
-        assert_eq!(fragchain.min, 0);
-        assert!(fragchain.maxp1 >= 0);
-
-        // convert into raw pointer and forget about it
-        let ptr = Box::into_raw(serdata);
-        // only we know this ddsi_serdata is really of type SerData
-        ptr as *mut ddsi_serdata
-    }
-
-    unsafe extern "C" fn free_serdata(serdata: *mut ddsi_serdata) {
-        // the pointer is really a *mut SerData
-        let ptr = serdata as *mut SerData;
-        let _data = Box::from_raw(ptr);
-        // _data goes out of scope and frees the SerData. Nothing more to do here.
-    }
+ 
 }
 
-/*
-void* data;
-    size_t data_size;
-    void* key;
-    size_t key_size;
-    ddsi_keyhash_t hash;
-    bool key_populated;
-    bool data_is_key;
- */
-
+/// A representation for the serialized data.
 #[repr(C)]
-struct SerData {
+struct SerData<T> {
     serdata: ddsi_serdata,
-    data_size: usize,
-    key_size: usize,
-    key_populated: bool,
-    data_is_key: bool,
+    maybe_sample : Option<std::sync::Arc<T>>,
+    key_hash : ddsi_keyhash,
 }
 
-impl SerData {
-    fn new(sertype: *const ddsi_sertype, kind: u32, data_size: usize) -> Box<SerData> {
-        Box::<SerData>::new(SerData {
+impl <T>SerData<T> {
+    fn new(sertype: *const ddsi_sertype, kind: u32) -> Box<SerData<T>> {
+        Box::<SerData<T>>::new(SerData {
             serdata: {
                 let mut data = std::mem::MaybeUninit::uninit();
                 unsafe {
@@ -288,51 +319,124 @@ impl SerData {
                     data.assume_init()
                 }
             },
-            data_size,
-            key_size: 0,
-            key_populated: false,
-            data_is_key: false,
+            maybe_sample : None,
+            key_hash : ddsi_keyhash::default()
         })
     }
 }
 
-/*
-struct FragChain<'a>{chain: &'a nn_rdata, offset:u32, current:&'a nn_rdata}
+/*  These functions are created from the macros in 
+    https://github.com/eclipse-cyclonedds/cyclonedds/blob/f879dc0ef56eb00857c0cbb66ee87c577ff527e8/src/core/ddsi/include/dds/ddsi/q_radmin.h#L108
+    Bad things will happen if these macros change. 
+    Some discussions here: https://github.com/eclipse-cyclonedds/cyclonedds/issues/830
+*/
+fn nn_rdata_payload_offset(rdata: *const nn_rdata) -> usize {
+    unsafe {(&*rdata).payload_zoff as usize} 
+}
 
-impl <'a>Iterator for FragChain<'a> {
-    type Item = &'a [u8];
-    fn next(&mut self) -> Option<Self::Item> {
-
-        if self.chain.maxp1 > self.offset {
-            let payload = get_payload_with_offset(self.current.rmsg, o)
-        }
-
-        // Since there's no endpoint to a Fibonacci sequence, the `Iterator`
-        // will never return `None`, and `Some` is always returned.
-        //Some(self.curr)
-        None
+fn nn_rmsg_payload(rmsg : *const nn_rmsg) -> * const u8{
+    unsafe {
+        rmsg.add(1) as *const u8
     }
 }
 
-// Warning: These functions are derived from the macros
-// in dds/ddsi/q_radmin.h.
-//#define NN_RMSG_PAYLOAD(m) ((unsigned char *) (m + 1))
-//#define NN_RMSG_PAYLOADOFF(m, o) (NN_RMSG_PAYLOAD (m) + (o))
-// All this will break if these macros change.
-// TODO: figure out a way to check this at compile time?
-fn get_payload(msg:&[u8]) -> &[u8] {
-    &msg[1..]
+fn nn_rmsg_payload_offset(rmsg : *const nn_rmsg, offset: usize) -> * const u8{
+    unsafe {
+        nn_rmsg_payload(rmsg).add(offset)
+    }
 }
 
-fn get_payload_with_offset(msg:&[u8], o: usize) -> &[u8] {
-    &get_payload(msg)[o..]
+
+
+/// A reader for a list of scatter gather buffers
+struct SGReader<'a> {
+    sc_list: Option<Vec<&'a [u8]>>,
+    //the current slice that is used
+    slice_cursor: usize,
+    //the current offset within the slice
+    slice_offset: usize,
 }
 
-*/
+impl<'a> SGReader<'a> {
+    pub fn new(sc_list: Vec<&'a [u8]>) -> Self {
+        SGReader {
+            sc_list: Some(sc_list),
+            slice_cursor: 0,
+            slice_offset: 0,
+        }
+    }
+}
+
+impl<'a> Read for SGReader<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let read_buf_len = buf.len();
+        if self.sc_list.is_some() {
+            let source_slice = self.sc_list.as_ref().unwrap()[self.slice_cursor];
+            let num_slices = self.sc_list.as_ref().unwrap().len();
+            let source_slice_rem = source_slice.len() - self.slice_offset;
+            let source_slice = &source_slice[self.slice_offset..];
+
+            let copy_length = std::cmp::min(source_slice_rem, read_buf_len);
+
+            //copy the bytes, lengths have to be the same
+            buf[..copy_length].copy_from_slice(&source_slice[..copy_length]);
+
+            if copy_length == source_slice_rem {
+                // we have completed this slice. move to the next
+                self.slice_cursor += 1;
+                self.slice_offset = 0;
+
+                if self.slice_cursor >= num_slices {
+                    //no more slices, invalidate the sc_list
+                    let _ = self.sc_list.take();
+                } 
+                
+            } else {
+                // we have not completed the current slice, just bump up the slice offset
+                self.slice_offset += copy_length;
+            }
+
+            Ok(copy_length)
+        } else {
+            // No more data
+            Ok(0)
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
+    #[test]
+    fn scatter_gather() {
+        let a = vec![1, 2, 3, 4, 5, 6];
+        let b = vec![7, 8, 9, 10, 11];
+        let c = vec![12, 13, 14, 15];
+        let d = vec![16, 17, 18, 19, 20, 21];
+
+        let sla = unsafe { std::slice::from_raw_parts(a.as_ptr(), a.len()) };
+        let slb = unsafe { std::slice::from_raw_parts(b.as_ptr(), b.len()) };
+        let slc = unsafe { std::slice::from_raw_parts(c.as_ptr(), c.len()) };
+        let sld = unsafe { std::slice::from_raw_parts(d.as_ptr(), d.len()) };
+
+        let sc_list = vec![sla, slb, slc, sld];
+
+        let mut reader = SGReader::new(sc_list);
+
+        let mut buf = vec![0,0,0,0,0];
+        if  let Ok(n) = reader.read(&mut buf) {
+            assert_eq!(&buf[..n],vec![1, 2, 3, 4, 5]);
+        } else {
+            panic!("should not panic");
+        }
+        if  let Ok(n) = reader.read(&mut buf) {
+            assert_eq!(&buf[..n],vec![6]);
+        } else {
+            panic!("should not panic");
+        }
+        
+    }
+
     #[test]
     fn basic() {
         let t = TopicStruct::create_type();
